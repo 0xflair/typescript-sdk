9d1b201d01f3c7126b696b5d2b6ceb655dc99972
kasrakhosravi <kasrakhosravi2@gmail.com>
2022-08-02 22:13:23 +0200

    add okc chain to flair_chains

--- packages/react-wallet/src/constants/chains.ts
+++ packages/react-wallet/src/constants/chains.ts
@@ -1,6 +1,29 @@
 import { allChains as WagmiChains, Chain } from 'wagmi';
 
-export const FLAIR_CHAINS: Chain[] = WagmiChains;
+// TODO: Move these to wagmi repo itself
+// https://github.com/wagmi-dev/wagmi/blob/0.3.x/packages/core/src/constants/chains.ts#L234
+const newChains = [
+  {
+    id: 66,
+    name: 'OKC Mainnet',
+    nativeCurrency: { name: 'OKT', symbol: 'OKT', decimals: 18 },
+    rpcUrls: {
+      default: 'https://exchainrpc.okex.org',
+    },
+    blockExplorers: {
+      etherscan: {
+        name: 'Etherscan',
+        url: 'https://www.oklink.com/okc/',
+      },
+      default: {
+        name: 'default',
+        url: 'https://www.oklink.com/okc/',
+      },
+    },
+  },
+];
+
+export const FLAIR_CHAINS: Chain[] = [...WagmiChains, ...newChains];
 
 export const FLAIR_DEFAULT_CHAIN = FLAIR_CHAINS.find(
   (c) => c.id === 4 /* rinkeby */,
